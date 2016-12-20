0b6d71a66dc291907ec7680cc7bfab28e5d2bb78
jiang2120 <jiang2120@163.com>
2016-12-20 17:28:52 +0800

    hello2

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,7 +1,8 @@
 
 public class HelloWorld {
 	public static void main(String args[]){
-		System.out.println("Helloword");
+		//System.out.println("Helloword");
+		System.out.println("hello");
 	}
 
 }
